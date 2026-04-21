79d7547a995504beda59e079da372e65689d3ea6
Duke <ducanup01@gmail.com>
2026-04-21 22:11:31 +0700

    making mazes too big to solve

--- src/main.c
+++ src/main.c
@@ -4,27 +4,38 @@
 #include "maze.h"
 #include "mazeRunner.h"
 #include "stack.h"
+#include <time.h>
 
 void main()
 {
-    // Maze *myMaze = generateMaze(45, 120);
-    Maze *myMaze = loadMazeBinary("../saved_mazes/maze45x120.bin");
+
+    // Maze *myMaze = generateMaze(8000, 8000);
+
+    Maze *myMaze = loadMazeBinary("../saved_mazes/maze1Kx1K.bin");
+    Node* startingPoint = buildGraph(myMaze);
+
     if (!myMaze)
     {
         printf("Failed to load maze\n");
         exit(1);
     }
 
-    Node* startingPoint = buildGraph(myMaze);
-
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
     mazeTraverse(startingPoint);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    long seconds = end.tv_sec - start.tv_sec;
+    long nanoseconds = end.tv_nsec - start.tv_nsec;
+    long total_ns = seconds * 1000000000L + nanoseconds;
+    printf("Time taken: %ld\n", total_ns);
 
     // printMaze(myMaze);
 
     // don't do this:
     // printf("%d\n", startingPoint->down->visited);
 
-    // saveMazeBinary(myMaze, "maze45x120.bin");
+    // saveMazeBinary(myMaze, "maze8Kx8K.bin");
 
     freeMaze(myMaze);
 
